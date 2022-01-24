5a4fbf84cf17ea9722843add705c1f98ab14edd3
Afina <afinawalton95@gmail.com>
2022-01-24 11:46:57 -0800

    Connects Postgres db to Express server

---
+++ server/app/models/index.js
@@ -0,0 +1,33 @@
+const dbConfig = require('../config/db.config.js');
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: dbConfig.USER,
+    host: dbConfig.HOST,
+    database: dbConfig.DB,
+    password: dbConfig.PASSWORD,
+    port: 5432
+})
+
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//     host: dbConfig.HOST,
+//     dialect: dbConfig.dialect,
+//     operatorsAliases: false,
+
+//     pool: {
+//         max: dbConfig.pool.max,
+//         min: dbConfig.pool.min,
+//         acquire: dbConfig.pool.acquire,
+//         idle: dbConfig.pool.idle
+//     }
+// });
+
+// const db = {};
+
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
+
+// db.users = require('./user.model.js')(sequelize, Sequelize);
+
+module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,28 @@
 const express = require('express');
-const bodyParse = require('body-parser');
-const cors = require('cors');
+const bodyParser = require('body-parser');
+const { response } = require('express');
+// const cors = require('cors');
 
 const app = express();
+const db = require('./app/models');
+const PORT = 3000;
 
-const corsOptions = {
-    origin: 'http://localhost:8081'
-};
+// const corsOptions = {
+//     origin: 'http://localhost:8081'
+// };
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: true }));
+
+
+// db.sequelize.sync();
 
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to Unfreeze Seattle's API!" });
 });
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
