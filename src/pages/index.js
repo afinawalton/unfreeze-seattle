72819bd0583808fdeeecd5daeab4172efa7e75f0
Afina <afinawalton95@gmail.com>
2022-01-27 14:45:30 -0800

    Adds login callback to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginForm from '../components/LoginForm.js';
+const axios = require('axios');
 
 const Home = () => {
+    // Might need user in useState
+    const [currentUser, setCurrentUser] = useState(null);
+
+    const logInUser = (user) => {
+        console.log(`User details: ${user}`);
+    
+        axios.post('http://localhost:8080/session', user)
+            .then(res => {
+                console.log('Server response', res);
+                setCurrentUser(user);
+                console.log(`Logged in user is ${user.email}`);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        // setCurrentUser(user);
+        // Sets up private route once user is logged in
+    }
+
     return (
         <main>
             <header id='titleCard'>
-                <h1><span class="green">Unfreeze</span> <span class="blue">Seattle</span></h1>
+                <h1><span className="green">Unfreeze</span> <span className="blue">Seattle</span></h1>
                 <div>Making friends just got a hell of a lot easier</div>
             </header>
-            <form id='loginForm'>
-                <p>
-                    <label for='email'>Email address</label>
-                    <input type='text' id='email' name='email_address' placeholder='Email' required></input>
-                </p>
-                <p>
-                    <label>Password</label>
-                    <input type='text' id='password' name='password_entry' placeholder='Password' required></input>
-                </p>
-                <p>
-                    <button type='submit' id='loginButton'>Log In</button>
-                </p>
-            </form>
+            <LoginForm logInUserCallback={logInUser} />
             <p id='noAccount'>Don't have an account?</p>
             <a id='createAccount' href='/sign-up'>Create an Account</a>
             <footer>
