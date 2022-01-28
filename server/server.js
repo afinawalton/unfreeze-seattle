280dfe745f93145d25ac5581f1e46685dffeba5b
Afina <afinawalton95@gmail.com>
2022-01-28 08:13:00 -0800

    Removes CORS options

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 
 const app = express();
 
-const corsOptions = {
-    origin: 'http://localhost:8081'
-};
+// const corsOptions = {
+//     origin: 'http://localhost:8081'
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
