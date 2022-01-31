dcedbfe1a2e00712df56b4ee34e5215ead6b87e9
Afina <afinawalton95@gmail.com>
2022-01-31 14:40:39 -0800

    Renders neighborhoods dynamically from API

--- src/pages/neighbhorhoods.js
+++ src/pages/neighbhorhoods.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Neighborhood from '../components/Neighborhood';
+const axios = require('axios');
 
 const Neighborhoods = () => {
+    const [neighbhorhoods, setNeighborhoods] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/neighborhoods')
+        .then(res => {
+            let list = [];
+            const neighborhoodData = res.data;
+            for (let item of neighborhoodData) {
+                list.push(item);
+            };
+            setNeighborhoods(list);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+    // Create IndividualNeighborhodood component to redirect to
+    // Import browser router to route to different pages
+    // Use structure in App.js
+    // Back-end needs to use Yelp API
+    // Or I need to add the best data myself
+
+    // When you click on a neighborhood
+    // It stores that context in state
+    // Passes props to neighborhood component
+    // Props: list of must eat, list of must do, list of must see
+    // Renders a page with that neighborhood component
+    const normalizeName = (name) => {
+        name = name.replace(/\s/g, '-').toLowerCase();
+        return name
+    }
+    
     return (
         <main>
             <h1>Neighborhoods</h1>
-            <h2>Seattle</h2>
-            <section id="neighborhoodsList">
-                <ul> list of neighbhorhoods
-                    <li>Ballard</li>
-                    <li>Beacon Hill</li>
-                    <li>Belltown</li>
-                    <li>Capitol Hill</li>
-                    <li>Central District</li>
-                    <li>Columbia City</li>
-                    <li>Crown Hill</li>
-                    <li>Downtown Seattle</li>
+            <section id="neighborhoodContainer">
+                <ul id='neighborhoodList'>
+                    {neighbhorhoods.map(info => <li key={info.name}><a href={normalizeName(info.name)}>{info.name}</a></li>)}
                 </ul>
             </section>
             <Neighborhood />
