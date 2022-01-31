5bee0947de2c545c1f7749b714b6ea407a991049
Afina <afinawalton95@gmail.com>
2022-01-30 16:26:42 -0800

    Updates nav bar depending on active user

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useAuth } from '../use-auth';
 
-const NavBar = () => {
+const NavBar = (props) => {
     // if (userLoggedIn = true) { display different NavBar }
+    const auth = useAuth();
+
+    // if auth.user, render the standard navbar
+    // if not, only display a login
+    const currentUser = auth.user ? 
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/main-feed">News Feed</a></li>
+        <li><a href="/my-profile">My Profile</a></li>
+        <li><a href="/neighborhoods">Neighborhoods</a></li>
+        <li><button onClick={() => auth.logout()}>Log Out</button></li>
+    </ul> :
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/neighborhoods">Neighborhoods</a></li>
+    </ul>
+
     return (
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/main-feed">News Feed</a></li>
-            <li><a href="/my-profile">My Profile</a></li>
-            <li><a href="/neighborhoods">Neighborhoods</a></li>
-            <li><a href="/log-out">Log Out</a></li>
-        </ul>
+        currentUser
     )
 }
 
