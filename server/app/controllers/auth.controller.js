2bd740267e9af3eee7bd6fd588c96abb81801906
Afina <afinawalton95@gmail.com>
2022-02-01 15:09:56 -0800

    Adds token functions to signup, login, and logout

--- server/app/controllers/auth.controller.js
+++ server/app/controllers/auth.controller.js
@@ -5,9 +5,48 @@ const Op = db.Sequelize.Op;
 
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const { promisify } = require('util');
 
-// Include user profile form on front-end
-exports.signUpUser = (req, res) => {
+// Sign JWT token for authenticated user
+const signToken = (id) => {
+    return jwt.sign({ id }, config.secret, {
+        expiresIn: 86400 // 24 hours
+    });
+}
+
+// Create JWT token for authenticated user
+const createUserToken = async (user, code, req, res) => {
+    const token = signToken(user.id);
+    console.log('Signing user token: ', token);
+    // Set expiry to 1 month
+    let d = new Date();
+    d.setDate(d.getDate() + 30);
+
+    // First-party cookie settings
+    // Creates a signed token
+    // Adds jwt as a key on cookie object? and passes signed token as the value
+    
+    // This isn't working vvvv
+    res.cookie('jwt', token, {
+        expires: d,
+        httpOnly: true,
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+        sameSite: 'none'
+    });
+    console.log('JWT =', res.cookie.jwt);
+
+    // Remove user password from output for security
+    user.password = undefined;
+    res.status(code).json({
+        status: 'success',
+        accessToken: token,
+        data: {
+            user
+        }
+    });
+};
+
+exports.signUpUser = async (req, res, next) => {
     if (!req.body.email) {
         missing = 'Email';
         res.status(400).send({
@@ -65,23 +104,23 @@ exports.signUpUser = (req, res) => {
         }]
     })
         .then(user => {
-            // Create token
-            // token will be added to user data
-
-            let token = jwt.sign({ id: user.id }, config.secret, {
-                expiresIn: 86400 // 24 hours
-            });
+            // // Create token
+            // // token will be added to user data
+            createUserToken(user, 201, req, res);
+            // let token = jwt.sign({ id: user.id }, config.secret, {
+            //     expiresIn: 86400 // 24 hours
+            // });
 
             // Return new user
-            res.status(201).send({
-                id: user.id,
-                email: user.email,
-                birthdate: user.birthdate,
-                resident_type: user.resident_type,
-                years_in_wa: user.years_in_wa,
-                user_profile: user.user_profile,
-                accessToken: token
-            });
+            // res.status(201).send({
+            //     id: user.id,
+            //     email: user.email,
+            //     birthdate: user.birthdate,
+            //     resident_type: user.resident_type,
+            //     years_in_wa: user.years_in_wa,
+            //     user_profile: user.user_profile,
+            //     accessToken: token
+            // });
         })
         .catch(err => {
             console.log(err);
@@ -91,7 +130,7 @@ exports.signUpUser = (req, res) => {
         });
 };
 
-exports.logInUser = (req, res) => {
+exports.logInUser = (req, res, next) => {
     User.findOne({
         where: {
             email: req.body.email
@@ -111,19 +150,20 @@ exports.logInUser = (req, res) => {
                 });
             }
 
-            let token = jwt.sign({ id: user.id }, config.secret, {
-                expiresIn: 86400 // 24 hours
-            });
+            // let token = jwt.sign({ id: user.id }, config.secret, {
+            //     expiresIn: 86400 // 24 hours
+            // });
 
-            res.status(200).send({
-                user: user.id,
-                email: user.email,
-                birthdate: user.birthdate,
-                resident_type: user.resident_type,
-                years_in_wa: user.years_in_wa,
-                user_profile: user.user_profile,
-                accessToken: token
-            });
+            createUserToken(user, 200, req, res);
+            // res.status(200).send({
+            //     user: user.id,
+            //     email: user.email,
+            //     birthdate: user.birthdate,
+            //     resident_type: user.resident_type,
+            //     years_in_wa: user.years_in_wa,
+            //     user_profile: user.user_profile,
+            //     accessToken: token
+            // });
         })
         .catch(err => {
             res.status(500).send({ message: err.message });
@@ -131,24 +171,25 @@ exports.logInUser = (req, res) => {
 };
 
 //check if user is logged in
-// exports.checkUser = async (req, res, next) => {
-//     let currentUser;
-//     if (req.cookies.jwt) {
-//        const token = req.cookies.jwt;
+exports.checkUser = async (req, res, next) => {
+    let currentUser;
+    if (req.signedCookies.jwt) {
+       const token = req.cookies.jwt;
 
-//        const decoded = await promisify(jwt.verify)(token, 'unfreeze-seattle-secret-key');
+       const decoded = await promisify(jwt.verify)(token, config.secret);
 
-//        currentUser = await User.findByPk(decoded.id);
-//    } else {
-//      currentUser =  null;
-//   }
-//    res.status(200).send({ currentUser });
-// };
+       currentUser = await User.findByPk(decoded.id);
+   } else {
+     currentUser =  null;
+  }
+    res.status(200).send({ currentUser });
+};
 
 // //log user out
-// exports.logoutUser = catchAsync(async (req, res) => {
-//     res.cookie('jwt', 'loggedout', {
-//     expires: new Date(Date.now() + 10 * 1000),
-//     httpOnly: true
-// });    res.status(200).send('user is logged out');
-// });
+exports.logoutUser = async (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+    res.status(200).send('User successfully logged out!');
+};
