d433046f0af4041b7b073cf07d97fc9655f7bee3
Afina <afinawalton95@gmail.com>
2022-02-01 10:19:55 -0800

    Routes MainFeed to homepage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ProvideAuth } from './hooks/useAuth';
 // import useFindUser from './hooks/useFindUser';
 // COMPONENTS
 import NavBar from './components/NavBar';
-import Home from './pages';
+// import Home from './pages';
 import CreateAccount from './pages/CreateAccount';
 import MyProfile from './pages/MyProfile';
 import MainFeed from './pages/MainFeed';
@@ -21,10 +21,10 @@ const App = () =>{
           <NavBar />
           {/* <UserProfile /> */}
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<MainFeed />} />
             <Route path='/create-account' element={<CreateAccount />} />
             <Route path='/my-profile' element={<MyProfile />} />
-            <Route path='/main-feed' element={<MainFeed />} />
+            {/* <Route path='/main-feed' element={<MainFeed />} /> */}
             <Route path='/neighborhoods' element={<Neighborhoods />} />
             {/* <Route path='/log-out' element={<LogOut />} /> */}
           </Routes>
--- src/pages/mainFeed.js
+++ src/pages/mainFeed.js
@@ -5,31 +5,37 @@ import { useAuth } from '../hooks/useAuth';
 const MainFeed = () => {
     const auth = useAuth();
 
-    const aside = auth.user.resident_type === 'local' ?
+    const aside =
+    // auth.user.resident_type === 'local' ?
     <aside id='addYourFavs'>
         <h2>Add Your Favs</h2>
         <p>|Know where to find the best bookstore? Or poke shop? Add your favorites to our database.|</p>
-    </aside> :
-    <aside id='didYouKnow'>
-        <h2>Did You Know?</h2>
-        <p>|Pike Place Market is the oldest continuously operating farmer’s market in the country.|</p>
     </aside>
+    // :
+    // <aside id='didYouKnow'>
+    //     <h2>Did You Know?</h2>
+    //     <p>|Pike Place Market is the oldest continuously operating farmer’s market in the country.|</p>
+    // </aside>
 
-    const feed = auth.user.resident_type === 'local' ?
+    const feed =
+    // auth.user.resident_type === 'local' ?
     <section id='blurbFeed'>
         <h2>Show a transplant around town:</h2>
         <Blurb />
-    </section> :
-    <section id='blurbFeed'>
-        <h2>Get to know the locals:</h2>
-        <Blurb />
     </section>
+    // :
+    // <section id='blurbFeed'>
+    //     <h2>Get to know the locals:</h2>
+    //     <Blurb />
+    // </section>
 
     return (
         <main>
             <section id="loggedInAs">
                 <img src='' alt='profilePic'></img>
-                <p>Logged in as <br /><span id="user">{auth.user.first_name}</span></p>
+                <p>Logged in as <br /><span id="user">
+                    {/* {auth.user.first_name} */}
+                </span></p>
                 <p>find a hiking trail on weekend mornings</p>
                 <button>Edit My Blurb</button>
             </section>
