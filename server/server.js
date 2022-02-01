504ccfa77e626c14315e79f9e443b118c6861197
Afina <afinawalton95@gmail.com>
2022-02-01 15:09:00 -0800

    Adds cookie parser to Express server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 const db = require('./app/models');
 db.sequelize.sync({ force: true })
