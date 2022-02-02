c23030a144193a2c6eba52a31b8853736b20ffc1
Afina <afinawalton95@gmail.com>
2022-02-02 15:28:34 -0800

    Removes unused comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { ProvideAuth } from './hooks/useAuth';
-// import { useAuth } from './hooks/useAuth';
 import useFindUser from './hooks/useFindUser';
 import { UserContext } from './hooks/UserContext';
 import PrivateRoute from './pages/PrivateRoute';
@@ -12,19 +10,15 @@ import CreateAccount from './pages/CreateAccount';
 import MyProfile from './pages/MyProfile';
 import Neighborhoods from './pages/Neighbhorhoods';
 import EditProfile from './pages/EditProfile';
-// const axios = require('axios');
 
 const App = () =>{
   const { user, setUser, isLoading } = useFindUser();
-  // const auth = useAuth();
 
   return (
     <main className="App">
-      {/* <ProvideAuth> */}
         <Router>
           <UserContext.Provider value={{ user, setUser, isLoading }}>
             <NavBar />
-            {/* <UserProfile /> */}
             <Routes>
               <Route path='/' element={<PrivateRoute />}></Route>
               <Route path='/login' element={<Login />}></Route>
@@ -35,7 +29,6 @@ const App = () =>{
             </Routes>
           </UserContext.Provider>
         </Router>
-      {/* </ProvideAuth> */}
     </main>
   );
 }
