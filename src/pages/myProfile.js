86275e5e0aa270e29852e0eedff8d5f26c9e0287
Afina <afinawalton95@gmail.com>
2022-02-03 14:06:51 -0800

    Renders info belonging to particular user

--- src/pages/myProfile.js
+++ src/pages/myProfile.js
@@ -3,9 +3,9 @@ import { UserContext } from '../hooks/UserContext';
 
 const MyProfile = () => {
     const { user } = useContext(UserContext);
-    const type = 'local';
+    const profile = user['user_profile'];
 
-    const checklist = type === 'local' ?
+    const checklist = user['resident_type'] === 'local' ?
     <section id='mustHaves'>
         <div id='mustEat'>
             <h2>Must Eat</h2>
@@ -34,25 +34,25 @@ const MyProfile = () => {
     </section> :
     null
 
-    const prompts = type === 'local' ?
+    const prompts = user['resident_type'] === 'local' ?
     <section id='profilePrompts'>
         <div>
             <h2>What’s your favorite thing about Seattle?</h2>
-            <p>|answer1|</p>
+            <p>{profile['prompt_answers']['prompt 1']}</p>
         </div>
         <div>
             <h2>What makes you stay here?</h2>
-            <p>|answer2|</p>
+            <p>{profile['prompt_answers']['prompt 2']}</p>
         </div>
-    </section> : type === 'transplant' ?
+    </section> : user.resident_type === 'transplant' ?
     <section id='profilePrompts'>
         <div>
             <h2>Why did you move to Seattle?</h2>
-            <p>|answer1|</p>
+            <p>{profile['prompt_answers']['prompt 1']}</p>
         </div>
         <div>
             <h2>What do you most want to explore in WA?</h2>
-            <p>|answer2|</p>
+            <p>{profile['prompt_answers']['prompt 2']}</p>
         </div>
     </section> : null
 
@@ -61,23 +61,20 @@ const MyProfile = () => {
             <button>Edit Profile</button>
             <section id='profileHeader'>
                 <img src='' alt='Portrait of {type}' />
-                <p>|name|, |age|, |pronouns|</p>
-                <p>|city|, |neighborhood|</p>
+                <p>{profile['first_name']} {user['birthdate']} {profile['pronouns']}</p>
+                <p>{profile['city']} - {profile['neighborhood']}</p>
                 <div>
                     <section id="topInterest">
-                        <p>|topInterest|</p>
-                        <p>|profile.blurb|</p>
+                        <p>{profile['top_interest']}</p>
+                        <p>{profile['blurb']}</p>
                     </section>
                     <section id='otherInterests'>
-                        <p>Foodie</p>
-                        <p>Breweries</p>
-                        <p>Techie</p>
+                        {profile['other_interests'].map(item => <p className='interest'>{item}</p>)}
                     </section>
                 </div>
-                    {/* section means grouped under single theme */}
             </section>
             <section id="profileBio">
-                <p>|bio|</p>
+                <p>{profile['bio']}</p>
             </section>
             {checklist}
             {prompts}
