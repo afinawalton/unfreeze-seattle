cc4e383d9dd8438455be411877389aaddd177045
Afina <afinawalton95@gmail.com>
2022-02-03 12:15:47 -0800

    Properly sets user to response.data

--- src/hooks/useFindUser.js
+++ src/hooks/useFindUser.js
@@ -5,11 +5,14 @@ export default function useFindUser() {
     const [user, setUser] = useState(null);
     const [isLoading, setLoading] = useState(true);
 
+    // Now this hook is messing up our cookie
     useEffect(() => {
         async function findUser() {
             await axios.get('http://localhost:8080/user', { withCredentials: true })
             .then(res => {
-                setUser(res.data.currentUser);
+                console.log('This is the data we got back from findUser() ', res);
+                setUser(res.data);
+                console.log('Found user: ', user);
                 setLoading(false);
             })
             .catch(err => {
@@ -23,6 +26,7 @@ export default function useFindUser() {
 
     return {
         user,
+        setUser,
         isLoading
     }
 }
