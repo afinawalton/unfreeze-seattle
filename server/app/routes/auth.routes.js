52961356b90559720c65b7e3a48b81aabb677ba5
Afina <afinawalton95@gmail.com>
2022-02-03 11:17:38 -0800

    Allows credentials and origin of client

--- server/app/routes/auth.routes.js
+++ server/app/routes/auth.routes.js
@@ -4,7 +4,8 @@ const { router } = require('../../server');
 
 module.exports = (app) => {
     app.use((req, res, next) => {
-        // res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', `${req.protocol}://${req.hostname}:3000`);
         res.header(
             'Access-Control-Allow-Headers',
             'x-access-token, Origin, Content-Type, Accept'
