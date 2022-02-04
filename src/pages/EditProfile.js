43436f600ff62b3f426df6f75e97246264265667
Afina <afinawalton95@gmail.com>
2022-02-04 15:18:49 -0800

    Adds neighborhoods to state

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,56 +1,55 @@
 import React, { useState, useEffect, useContext } from 'react';
-// import useAuth from '../hooks/useAuth';
+import useAuth from '../hooks/useAuth';
 import { UserContext } from '../hooks/UserContext';
 const axios = require('axios');
 
 const EditProfile = () => {
-    const { user, editProfile } = useContext(UserContext);
+    const { user } = useContext(UserContext);
+    const { editProfile } = useAuth();
 
     const [interests, setInterests] = useState([]);
+    const [neighborhoods, setNeighborhoods] = useState([]);
 
     // Get list of interests from database and add to state
     useEffect(() => {
+        const controller = new AbortController();
+
         axios.get('http://localhost:8080/api/interests')
-            .then(res => {
-                // const interestList = res.data;
-                let interestList = [];
-                for (let item of res.data) {
-                    interestList.push(item.name);
-                }
-                setInterests(interestList);
-            })
+        .then(res => {
+            setInterests(res.data);
+        })
+        .catch(err => {
+            if (err.name === 'AbortError') {
+                console.log('Successfully aborted');
+            }
+            console.log(err);
+        });
+        return () => {
+            controller.abort();
+        };
     }, [])
 
     // Get list of neighborhoods from database and add to HTML
     // Use this in the user profile form after user account info is validated
     useEffect(() => {
+        const controller = new AbortController();
+
         axios.get('http://localhost:8080/api/neighborhoods')
-            .then(res => {
-                let neighborhoodList = res.data;
-                neighborhoodList.sort();
-                let selectNeighborhood = document.getElementById('neighborhoodList');
-                for (let item of neighborhoodList) {
-                    let neighborhood = document.createElement('option');
-                    neighborhood.value = item.name;
-                    neighborhood.textContent = item.name;
-                    selectNeighborhood.appendChild(neighborhood);
-                }
-            })
+        .then(res => {
+            let neighborhoodList = res.data;
+            neighborhoodList.sort();
+            setNeighborhoods(neighborhoodList);
+        })
+        .catch(err => {
+            if (err.name === 'AbortError') {
+                console.log('Successfully aborted');
+            }
+            console.log(err);
+        });
+        return () => {
+            controller.abort();
+        };
     }, [])
-
-    const currentUser = {
-        email: user.email,
-        birthdate: user.birthdate,
-        resident_type: user.resident_type,
-        years_in_wa: user.years_in_wa
-    }
-
-    // const currentUser = {
-    //     email: '',
-    //     birthdate: '',
-    //     resident_type: '',
-    //     years_in_wa: 0
-    // }
 
     const emptyProfile = {
         first_name: '',
@@ -85,6 +84,22 @@ const EditProfile = () => {
                 ...formFields,
                 other_interests: selectedInterests
             });
+        } else if (name === 'prompt 1') {
+            setFormFields({
+                ...formFields,
+                prompt_answers: {
+                    [name]: value,
+                    "prompt 2": formFields['prompt_answers']['prompt 2']
+                }
+            });
+        } else if (name === 'prompt 2') {
+            setFormFields({
+                ...formFields,
+                prompt_answers: {
+                    [name]: value,
+                    "prompt 1": formFields['prompt_answers']['prompt 1']
+                }
+            });
         }
         else {
             setFormFields({
@@ -97,11 +112,14 @@ const EditProfile = () => {
     const onFormSubmit = (e) => {
         e.preventDefault();
 
-        currentUser.user_profile = formFields
-
+        let currentUser = {};
+        // { user_id:, user_profile = { }}
+        currentUser.id = user.id;
+        currentUser.user_profile = formFields;
+        console.log(currentUser);
         editProfile(currentUser);
         
-        setFormFields(emptyProfile);
+        // setFormFields(emptyProfile);
     }
 
     return (
@@ -131,26 +149,29 @@ const EditProfile = () => {
                     <label htmlFor='neighborhood'>Neighborhood</label>
                     <select id='neighborhoodList' name='neighborhood' onChange={handleInputChange} value={formFields.neighborhood} >
                         <option value=''>Choose your closest neighborhood</option>
+                        {neighborhoods.map(pair => 
+                            <option key={pair['id']} value={pair['name']}>{pair['name']}</option>
+                        )}
                     </select>
                 </p>
                 <p>
                     <label htmlFor='top_interest'>Top Interest</label>
                     <select id='top_interest' name='top_interest' onChange={handleInputChange} value={formFields.top_interest} >
                         <option value=''>Choose your primary interest</option>
-                        {interests.map(name => 
-                            <option key={name} value={name}>{name}</option>
+                        {interests.map(pair => 
+                            <option key={pair['id']} value={pair['name']}>{pair['name']}</option>
                         )}
                     </select>
                 </p>
                 <p>
                     <label htmlFor='blurb'>Blurb</label>
-                    <input type="text" name="neighborhood" onChange={handleInputChange} value={formFields.blurb}></input>
+                    <input type="text" name="blurb" onChange={handleInputChange} value={formFields.blurb}></input>
                 </p>
                 <p>
                     <label htmlFor='other_interests'>Choose other interests</label>
-                    <select type='text' id='interests' name='interests' onChange={handleInputChange} multiple >
-                    {interests.map(name => 
-                            <option key={name} value={name}>{name}</option>
+                    <select type='text' id='interests' name='other_interests' onChange={handleInputChange} multiple >
+                    {interests.map(pair => 
+                            <option key={pair['id']} value={pair['name']}>{pair['name']}</option>
                         )}
                     </select>
                 </p>
@@ -177,11 +198,11 @@ const EditProfile = () => {
                     } */}
                 <p>
                     <label htmlFor='prompt1'>What are you most excited to explore in Seattle?</label>
-                    <input id='prompt1' value={formFields.prompt_answers['prompt 1']} onChange={handleInputChange} type='text'></input>
+                    <input id='prompt1' name='prompt 1' value={formFields.prompt_answers['prompt 1']} onChange={handleInputChange} type='text'></input>
                 </p>
                 <p>
                     <label htmlFor='prompt2'>Why did you move to Seattle?</label>
-                    <input id='prompt2' value={formFields.prompt_answers['prompt 2']} onChange={handleInputChange} type='text'></input>
+                    <input id='prompt2' name='prompt 2' value={formFields.prompt_answers['prompt 2']} onChange={handleInputChange} type='text'></input>
                 </p>
                 <p>
                     <button type='submit' id='createAccountButton'>Create Account</button>
