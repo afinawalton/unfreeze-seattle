43423cf324c0354a0e6b9c278cc5cbcdff73360a
Afina <afinawalton95@gmail.com>
2022-02-04 15:17:43 -0800

    Allows credentials on server

--- server/app/routes/user.routes.js
+++ server/app/routes/user.routes.js
@@ -6,7 +6,8 @@ module.exports = app => {
 
     // AUTHENTICATION ROUTES
     app.use((req, res, next) => {
-        // res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', `${req.protocol}://${req.hostname}:3000`);
         res.header(
             'Access-Control-Allow-Headers',
             'x-access-token, Origin, Content-Type, Accept'
@@ -14,30 +15,30 @@ module.exports = app => {
         next();
     });
 
-    app.get('/users/all', users.allAccess);
+    app.get('api/users/all', users.allAccess);
 
     app.get(
-        '/users/locals',
+        'api/users/locals',
         [authJwt.verifyToken, authJwt.isLocal],
         users.localBoard
     );
 
     app.get(
-        '/users/transplants',
+        'api/users/transplants',
         [authJwt.verifyToken, authJwt.isTransplant],
         users.transplantBoard
     );
 
-    app.put(
-        '/user',
-        users.updateUser
+    app.post(
+        'api/users/my-profile',
+        users.updateUserProfile
     );
 
     // Create a new user
     // router.post('/', users.createNewUser);
 
     // Get all users
-    // router.get('/', users.getUsersByResidency);
+    app.get('/users', users.getUsersByResidency);
 
     // Get one user by id
     // router.get('/:id', users.getOneUser);
