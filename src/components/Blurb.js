3f8bdb86afc1f9bed972931d29c607c1604a3e98
Afina <afinawalton95@gmail.com>
2022-02-05 10:00:24 -0800

    Improves organization of blurb component

--- src/components/Blurb.js
+++ src/components/Blurb.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Blurb.css';
 
-const Blurb = () => {
+const Blurb = ({ userProfile }) => {
     return (
-        <div>
-            <img src='' alt='profilePic'></img>
-            <h3>|name|, |age|</h3>
-            <p>see films and find new local coffee shops</p>
+        <div id='blurb'>
+            <img src={userProfile['profile_pic']} alt='profilePic' />
+            <div>
+                <h3><a href=''>{userProfile['first_name']}</a></h3>
+                <p>{userProfile['blurb']}</p>
+            </div>
         </div>
     )
 }
