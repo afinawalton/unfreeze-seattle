ea3cf1035569e9e921b9c7327bd7cebd1ed5e759
Afina <afinawalton95@gmail.com>
2022-02-07 15:04:14 -0800

    Adds state and API calls to get single user

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -1,14 +1,42 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../hooks/UserContext';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+const axios = require('axios');
 
+const calculateAge = (birthdate) => {
+    let bdayObj = new Date(birthdate);
+    let diff_ms = Date.now() - bdayObj.getTime();
+    let age_dt = new Date(diff_ms);
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970)
+}
 
 const UserProfile = () => {
-    const { user } = useContext(UserContext);
-    const type = 'local';
+    const { userId } = useParams();
+    const [thisUser, setThisUser] = useState({});
+    const [profile, setProfile] = useState({});
+    const navigate = useNavigate();
 
-    const checklist = type === 'local' ?
-    <section id='mustHaves'>
-        <div id='mustEat'>
+    // Do a GET request to request data from server and when this component first renders, it will store the data in state using matched params
+    
+    useEffect(() => {
+        axios.get(`http://localhost:8080/users/${parseInt(userId)}`)
+        .then(res => {
+            console.log(res.data);
+            setThisUser(res.data);
+            setProfile(res.data.user_profile);
+        })
+        .then(() => {
+            console.log(thisUser);
+            console.log(profile);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
+    const checklist = thisUser['resident_type'] === 'local' ?
+    <section id='experiences'>
+        <div id='toEat'>
             <h2>Must Eat</h2>
             <ul>
                 <li>|rec1|</li>
@@ -16,7 +44,7 @@ const UserProfile = () => {
                 <li>|rec3|</li>
             </ul>
         </div>
-        <div id='mustDo'>
+        <div id='toDo'>
             <h2>Must Do</h2>
             <ul>
                 <li>|rec1|</li>
@@ -24,7 +52,7 @@ const UserProfile = () => {
                 <li>|rec3|</li>
             </ul>
         </div>
-        <div id='mustSee'>
+        <div id='toSee'>
             <h2>Must See</h2>
             <ul>
                 <li>|rec1|</li>
@@ -33,46 +61,75 @@ const UserProfile = () => {
             </ul>
         </div>
     </section> :
-    null
+    <section id='experiences'>
+        <div id='toEat'>
+            <h2>To Eat</h2>
+            <ul>
+                <li>|rec1|</li>
+                <li>|rec2|</li>
+                <li>|rec3|</li>
+            </ul>
+        </div>
+        <div id='toDo'>
+            <h2>To Do</h2>
+            <ul>
+                <li>|rec1|</li>
+                <li>|rec2|</li>
+                <li>|rec3|</li>
+            </ul>
+        </div>
+        <div id='toSee'>
+            <h2>To See</h2>
+            <ul>
+                <li>|rec1|</li>
+                <li>|rec2|</li>
+                <li>|rec3|</li>
+            </ul>
+        </div>
+    </section>
 
-    const prompts = type === 'local' ?
+    const prompts = thisUser['resident_type'] === 'local' ?
     <section id='profilePrompts'>
         <div>
             <h2>What’s your favorite thing about Seattle?</h2>
-            <p>|answer1|</p>
+            {/* <p>{profile['prompt_answers']['prompt 1']}</p> */}
         </div>
         <div>
             <h2>What makes you stay here?</h2>
-            <p>|answer2|</p>
+            {/* <p>{profile['prompt_answers']['prompt 2']}</p> */}
         </div>
-    </section> : type === 'transplant' ?
+    </section> :
     <section id='profilePrompts'>
         <div>
             <h2>Why did you move to Seattle?</h2>
-            <p>|answer1|</p>
+            {/* <p>{profile['prompt_answers']['prompt 1']}</p> */}
         </div>
         <div>
             <h2>What do you most want to explore in WA?</h2>
-            <p>|answer2|</p>
+            {/* <p>{profile['prompt_answers']['prompt 2']}</p> */}
         </div>
-    </section> : null
+    </section>
 
     return (
         <main>
-            <button id='backButton'>Back to Feed</button>
             <section id='profileHeader'>
-                <img src='' alt='Profile picture of {user}' />
-                <p>|name|, |age|, |pronouns|</p>
-                <p>|neighborhood|</p>
+            <button onClick={() => navigate(-1)}>← Back to Feed</button>
+                <img src='' alt={`Portrait of ${profile['first_name']}`} />
+                <p>{profile['first_name']} {calculateAge(thisUser['birthdate'])} {profile['pronouns']}</p>
+                <p>Seattle ‣ {profile['neighborhood']}</p>
                 <div>
-                    <p>|topInterest|</p>
-                    <p>|profile.blurb|</p>
+                    <section id="topInterest">
+                        <p>{profile['top_interest']}</p>
+                        <p>{profile['blurb']}</p>
+                    </section>
+                    <section id='otherInterests'>
+                        {profile['other_interests'] ? profile['other_interests'].map(item => <p className='interest'>{item}</p>) : null}
+                    </section>
                 </div>
-                    {/* section means grouped under single theme */}
-                <button>Send an Email</button>
+                <a href={`mailto:${thisUser.email}`}>Send an Email</a>
             </section>
             <section id="profileBio">
-                <p>|bio|</p>
+                <p>{profile['bio']}</p>
             </section>
             {checklist}
             {prompts}
