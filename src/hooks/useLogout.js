924f5e349a1bbe3ef06b3de00d0745fc0da98d57
Afina <afinawalton95@gmail.com>
2022-02-07 10:32:51 -0800

    Sets user and redirects to login on logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,13 +1,20 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "./UserContext";
 const axios = require('axios');
 
 export default function useLogout() {
+    const { setUser } = useContext(UserContext);
+
     let navigate = useNavigate();
 
     const logoutUser = async () => {
         axios.get('http://localhost:8080/auth/logout')
         .then(res => {
             console.log(res);
+            setUser(null);
+        })
+        .then(() => {
             navigate('/');
         })
         .catch(err => {
