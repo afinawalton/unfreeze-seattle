5942a57ac18856cc6dd0ad5eeaca94c992bef6dd
Afina <afinawalton95@gmail.com>
2022-02-07 15:02:22 -0800

    Adds params to Route component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import useFindUser from './hooks/useFindUser';
 import { UserContext } from './hooks/UserContext';
-import PrivateRoute from './pages/PrivateRoute';
 // COMPONENTS
 import NavBar from './components/NavBar';
 import Login from './pages';
@@ -12,32 +11,34 @@ import Neighborhoods from './pages/Neighbhorhoods';
 import EditProfile from './pages/EditProfile';
 import MainFeed from './pages/MainFeed';
 import DeleteAccount from './pages/DeleteAccount';
+import UserProfile from './components/UserProfile';
 
 const App = () => {
   const { user, setUser, isLoading } = useFindUser();
 
   return (
     <main className="App">
-        <Router>
-          <UserContext.Provider value={{ user, setUser, isLoading }}>
-            <NavBar />
-            {
-              user ?
-              <Routes>
-                <Route path='/' element={<MainFeed />}></Route>
-                <Route path='/my-profile' element={<MyProfile />} />
-                <Route path='/edit-profile' element={<EditProfile />} />
-                <Route path='/delete-account' element={<DeleteAccount />} />
-                <Route path='/neighborhoods' element={<Neighborhoods />} />
-              </Routes>
-              :
-              <Routes>
-                <Route path='/' element={<Login />}></Route>
-                <Route path='/create-account' element={<CreateAccount />} />
-              </Routes>
-            }
-          </UserContext.Provider>
-        </Router>
+        <UserContext.Provider value={{ user, setUser, isLoading }}>
+          <NavBar />
+          {
+            user ?
+            <Routes>
+              <Route path='/' element={<MainFeed />}></Route>
+              <Route path='/my-profile' element={<MyProfile />} />
+              <Route path='/edit-profile' element={<EditProfile />} />
+              <Route path='/delete-account' element={<DeleteAccount />} />
+              <Route path='/neighborhoods' element={<Neighborhoods />} />
+              <Route path='/users'>
+                <Route path=":userId" element={<UserProfile users={allUsers} />} />
+              </Route>
+            </Routes>
+            :
+            <Routes>
+              <Route path='/' element={<Login />}></Route>
+              <Route path='/create-account' element={<CreateAccount />} />
+            </Routes>
+          }
+        </UserContext.Provider>
     </main>
   );
 }
