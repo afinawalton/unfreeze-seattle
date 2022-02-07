46df05cca3482d38279418bcedcd5bde2d14870b
Afina <afinawalton95@gmail.com>
2022-02-07 15:04:00 -0800

    Wraps links in NavLink components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 import { UserContext } from '../hooks/UserContext';
 import useLogout from './../hooks/useLogout';
 import './NavBar.css';
@@ -11,9 +12,9 @@ const NavBar = () => {
     <nav>
         <h1><span className="green">Unfreeze</span> <span className="blue">Seattle</span></h1>
         <ul>
-            <li><a href="/">News Feed</a></li>
-            <li><a href="/my-profile">My Profile</a></li>
-            <li><a href="/neighborhoods">Neighborhoods</a></li>
+            <li><NavLink to='/'>News Feed</NavLink></li>
+            <li><NavLink to="/my-profile">My Profile</NavLink></li>
+            <li><NavLink to='/neighborhoods'>Neighborhoods</NavLink></li>
             <li><button id='logoutButton' onClick={() => logoutUser()}>Log Out</button></li>
         </ul>
     </nav> :
