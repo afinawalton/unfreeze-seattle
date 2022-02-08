4debb860a3f6871ed8d817b4f0487042043a92a3
Afina <afinawalton95@gmail.com>
2022-02-08 13:07:19 -0800

    Updates cleanup function to find user

--- src/hooks/useFindUser.js
+++ src/hooks/useFindUser.js
@@ -8,21 +8,25 @@ export default function useFindUser() {
     // Now this hook is messing up our cookie
     useEffect(() => {
         const controller = new AbortController();
-        // const signal = controller.signal;
-        // async function findUser() {
-        axios.get('http://localhost:8080/user', { withCredentials: true })
-        .then(res => {
-            console.log('This is the data we got back from findUser() ', res);
-            setUser(res.data);
-            setLoading(false);
-        })
-        .catch(err => {
-            if (err.name === 'AbortError') {
-                console.log('Successfully aborted');
-            }
-            setLoading(false);
-            console.log(err);
-        });
+
+        const findUser = async () => {
+            return await axios.get('http://localhost:8080/user', { withCredentials: true, signal: controller.signal })
+            .then(res => {
+                // console.log('This is the data we got back from findUser() ', res);
+                setUser(res.data);
+                setLoading(false);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('Successfully aborted');
+                }
+                setLoading(false);
+                console.log(err);
+            });
+        }
+
+        findUser();
+        
         return () => {
             controller.abort();
         };
