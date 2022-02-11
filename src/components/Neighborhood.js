273a355e37fff74ad82faeb3d5957ee4d2a84a2f
Afina <afinawalton95@gmail.com>
2022-02-11 14:43:24 -0800

    Populates neighborhood page

--- src/components/Neighborhood.js
+++ src/components/Neighborhood.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import './Neighborhood.css';
+import axios from 'axios';
 
 const Neighborhood = () => {
-    // const [selectedNeighborhood, setSelectedNeighborhood] = useState(null)
+    const { townId } = useParams();
+    const [thisTown, setThisTown] = useState({});
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/neighborhoods/${parseInt(townId)}`, { withCredentials: true })
+        .then(res => {
+            console.log(res.data);
+            setThisTown(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
 
     return (
-        <main>
-            <div>Neighborhood:</div>
-            <h1>Ballard</h1>
+        <main id='neighborhoodPage'>
+            <button className='userButton backToButton' onClick={() => navigate(-1)}>← Back to Feed</button>
+            <h1>Neighborhood: <span class="townName">{thisTown.name}</span></h1>
             <section id='mustEat'>
                 <h2>Must Eat</h2>
                 <img src='' alt='rec1'></img>
