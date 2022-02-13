032f1b1626d4f9895a84b923db2ac9111cda2f42
Afina <afinawalton95@gmail.com>
2022-02-13 13:06:16 -0800

    Updates sequelize config

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,18 +1,30 @@
-const dbConfig = require('../config/db.config.js');
+// const dbConfig = require('../config/db.config.js');
 
 const { Sequelize, Model } = require('sequelize');
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    // operatorsAliases: false,
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//     host: dbConfig.HOST,
+//     dialect: dbConfig.dialect,
+//     // operatorsAliases: false,
 
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
+//     pool: {
+//         max: dbConfig.pool.max,
+//         min: dbConfig.pool.min,
+//         acquire: dbConfig.pool.acquire,
+//         idle: dbConfig.pool.idle
+//     }
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-});
+  }
+);
 
 const db = {};
 
@@ -25,5 +37,14 @@ db.facts = require('./fact.model')(sequelize, Sequelize, Model);
 db.interests = require('./interest.model.js')(sequelize, Sequelize, Model);
 db.neighborhoods = require('./neighborhood.model')(sequelize, Sequelize, Model);
 db.data = require('./preloadedData.json');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = db;
